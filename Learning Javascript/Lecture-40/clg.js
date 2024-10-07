711558eddf4666d2415d9285682ddeb6a5399ede
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-10-07 11:57:44 +0530

    update

--- Learning Javascript/Lecture-40/clg.js
+++
@@ -1,5 +0,0 @@
-function MA(a, b, c) {
-    console.log(a+b+c) / 3;
-}
-
-MA(5, 10, 15);
---
+++ Learning Javascript/Lecture-40/index.js
@@ -0,0 +1,16 @@
+// 
+// var age = 25;
+
+// if (true) {
+//     console.log(age);
+// }
+
+
+function solve() {
+    var age = 25;
+    console.log(age)
+}
+
+solve();
+
+console.log(age)
