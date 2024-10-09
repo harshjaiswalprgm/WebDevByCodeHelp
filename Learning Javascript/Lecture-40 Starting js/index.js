e7a4bd9214226a80ff150f8ac853686a72ae1211
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-10-09 23:34:42 +0530

    update

---
+++ Learning Javascript/Lecture-40 Starting js/index.js
@@ -0,0 +1,51 @@
+// 
+// var age = 25;
+
+// if (true) {
+//     console.log(age);
+// }
+
+
+// function solve() {
+//     var age = 25;
+//     console.log(age)
+// }
+
+// solve();
+
+// console.log(age)
+
+// var x = 10;
+// var x = 20;
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// let a = 20;
+// a = 30;
+
+
+// let a = 10;
+// a = "bababr";
+// a = true;
+// a = null;
+
+// const a = 28;
+// console.log(a);
+
+// const a = 20;
+// console.log(a);
+
+
+// let marks = 20.2345;
+// marks = "Babbar";
+// marks = true;
+
+
+let marks = 565610681565145645554444444444444422222227;
+console.log(marks);
+
+let mark;
+console.log(typeof(mark));
